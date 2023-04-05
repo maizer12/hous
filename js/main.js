f8119ec5c51a313e464e68ef8a2af91b3be4f641
vasil <vasilbazhiv12@gmail.com>
2023-04-05 21:41:00 +0300

    new

---
+++ js/main.js
@@ -0,0 +1,15 @@
+new Swiper('.swiper', {
+	// Navigation arrows
+	navigation: {
+		nextEl: '.swiper-button-next',
+		prevEl: '.swiper-button-prev',
+	},
+	simulateTouch:false,
+	effect: 'fade',
+	fadeEffect:{
+		crossFade:true
+	},
+	// autoplay:{
+	// 	delay:3000
+	// }
+})
