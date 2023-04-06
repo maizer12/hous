fdb9e67f1d162b9ebb78258682414d5bc6c86dde
vasil <vasilbazhiv12@gmail.com>
2023-04-06 22:01:58 +0300

    version2.0

--- js/main.js
+++ js/main.js
@@ -1,14 +1,38 @@
-new Swiper('.swiper', {
+new Swiper('.home-slider__swiper', {
 	// Navigation arrows
+	loop: true,
 	navigation: {
-		nextEl: '.swiper-button-next',
-		prevEl: '.swiper-button-prev',
+		nextEl: '.swiper-home-btn_next',
+		prevEl: '.swiper-home-btn_prev',
 	},
 	simulateTouch:false,
 	effect: 'fade',
 	fadeEffect:{
 		crossFade:true
 	},
+	 autoplay:{
+	 	delay:3000
+	 }
+})
+new Swiper('.action__swiper', {
+	slidesPerView: 'auto',
+	loop: true,
+	navigation: {
+		nextEl: '.slider-header__button_next',
+		prevEl: '.slider-header__button_prev',
+	},
+	spaceBetween: 38,
+	// autoplay:{
+	// 	delay:3000
+	// }
+})
+new Swiper('.articles-slider', {
+	slidesPerView: 'auto',
+	navigation: {
+		nextEl: '.new-banners__button_next',
+		prevEl: '.new-banners__button_prev',
+	},
+	spaceBetween: 38,
 	// autoplay:{
 	// 	delay:3000
 	// }
