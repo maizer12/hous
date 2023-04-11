5c87571826e19c4f422aefcfab36ddb2211a6533
vasil <vasilbazhiv12@gmail.com>
2023-04-11 16:23:46 +0300

    new-js

--- js/header-footer.js
+++ js/header-footer.js
@@ -1,35 +1,60 @@
 $(function () {
-  var mq = window.matchMedia('(min-width: 769px)');
-  var accordion = $('#accordion').accordion({
-    active: null,
-    collapsible: true,
-    heightStyle: 'content',
-  });
+	var mq = window.matchMedia('(min-width: 769px)')
+	var accordion = $('#accordion').accordion({
+		active: null,
+		collapsible: true,
+		heightStyle: 'content',
+	})
 
-  function checkBreakpoint(mq) {
-    if (mq.matches) {
-      accordion.accordion('destroy');
-    } else {
-      accordion = $('#accordion').accordion({
-        active: null,
-        collapsible: true,
-        heightStyle: 'content',
-      });
-    }
-  }
+	function checkBreakpoint(mq) {
+		if (mq.matches) {
+			accordion.accordion('destroy')
+		} else {
+			accordion = $('#accordion').accordion({
+				active: null,
+				collapsible: true,
+				heightStyle: 'content',
+			})
+		}
+	}
 
-  mq.addListener(checkBreakpoint);
-  checkBreakpoint(mq);
-});
+	mq.addListener(checkBreakpoint)
+	checkBreakpoint(mq)
+})
 const labelSearch = document.querySelector('.header-content__label')
 
-labelSearch?.addEventListener('click', ()=>{
-  document.querySelector('.header-search')?.classList.toggle('header-search-open')
+labelSearch?.addEventListener('click', () => {
+	document
+		.querySelector('.header-search')
+		?.classList.toggle('header-search-open')
 })
 
 const burger = document.querySelector('.burger-menu')
+const body = document.querySelector('body')
+const btnSearch = document.querySelector('#search-btn')
+const btnContacts = document.querySelector('#btn-contacts')
+const addBodyClass = text => {
+	body.classList.add(text)
+}
 
-
-burger?.addEventListener('click', ()=>{
-  
+burger?.addEventListener('click', () => {
+	if (body.classList.contains('menu-open')) {
+		body.classList = ''
+	} else {
+		addBodyClass('menu-open')
+		addBodyClass('catalog-open')
+	}
+})
+btnSearch?.addEventListener('click', () => {
+	addBodyClass('menu-open')
+	addBodyClass('search-open')
+})
+btnContacts?.addEventListener('click', () => {
+	addBodyClass('menu-open')
+	addBodyClass('contacts-open')
+})
+$('#header-accordion').accordion({
+  active: null,
+  collapsible: true,
+  heightStyle: 'content',
 })
