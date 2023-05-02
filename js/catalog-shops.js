6db73ce2c64889f806c76e37fbb04b5f3cb902f7
vasil <vasilbazhiv12@gmail.com>
2023-05-02 17:42:50 +0300

    new-select

--- js/catalog-shops.js
+++ js/catalog-shops.js
@@ -1,3 +1,22 @@
 $(document).ready(function() {
 	$('.filter-item__select').select2();
 });
+const closeBtnFil = document.querySelector('.filter-wrapper__close')
+const btnOpenFil = document.querySelector('.shops-filter-open')
+const btnSubmitFil = document.querySelector('.filter-wrapper__submit')
+function checkFilNow(){
+	const checked = body.classList.contains('open-fil')
+	if(checked){
+		body.classList = ''
+	}
+}
+
+closeBtnFil?.addEventListener('click', ()=>{
+	checkFilNow()
+})
+btnOpenFil?.addEventListener('click', ()=>{
+	body.classList.add('open-fil')
+})
+btnSubmitFil?.addEventListener('click', ()=>{
+	checkFilNow()
+})
